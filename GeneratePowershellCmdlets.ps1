8beba2bbe79316a766cd8c73d83779ec3ef8f36a
EldoBam <amna.wolf@gmail.com>
2025-03-15 15:35:35 +0100

    added conduct and issue templates

--- GeneratePowershellCmdlets.ps1
+++ GeneratePowershellCmdlets.ps1
@@ -7,15 +7,16 @@ $templatepath = "$($PSScriptRoot)\templates\Powershell"
 # Clear output folder
 Get-ChildItem $apioutput -Exclude .git | Remove-Item -Recurse -Force
 
-Start-Process -FilePath $jdkPath -ArgumentList "-jar $jarpath  generate -g powershell -i $apiinput -o $apioutput -t $templatepath --package-name ProxmoxPVE --additional-properties=discardReadOnly=true,apiNamePrefix=PVE --skip-validate-spec" -wait
+Start-Process -FilePath $jdkPath -ArgumentList "-jar $jarpath  generate -g powershell -i $apiinput -o $apioutput -t $templatepath --package-name ProxmoxPVE --additional-properties=debugOperations=true,discardReadOnly=true,apiNamePrefix=PVE,useCmdletParams=true --skip-validate-spec" -wait
 # for debugging, open a CMD and insert the following command. Neccessary, if you need to see the output. --> Start-Process closes the window when finished
 "Command:"
-"$jdkPath -jar $jarpath generate -g powershell -i $apiinput -o $apioutput -t $templatepath --package-name ProxmoxPVE --additional-properties=discardReadOnly=true,apiNamePrefix=PVE --skip-validate-spec" | Set-Clipboard
-"$jdkPath -jar $jarpath generate -g powershell -i $apiinput -o $apioutput -t $templatepath --package-name ProxmoxPVE --additional-properties=discardReadOnly=true,apiNamePrefix=PVE --skip-validate-spec"
+"$jdkPath -jar $jarpath generate -g powershell -i $apiinput -o $apioutput -t $templatepath --package-name ProxmoxPVE --additional-properties=debugOperations=true,discardReadOnly=true,apiNamePrefix=PVE,useCmdletParams=true --skip-validate-spec" | Set-Clipboard
+"$jdkPath -jar $jarpath generate -g powershell -i $apiinput -o $apioutput -t $templatepath --package-name ProxmoxPVE --additional-properties=debugOperations=true,discardReadOnly=true,apiNamePrefix=PVE,useCmdletParams=true --skip-validate-spec"
 "copied to clipboard"
 "---"
 
 # copy license to directory
 Copy-Item "$($PSScriptRoot)\LICENSE" $apioutput
 Copy-Item "$($PSScriptRoot)\CONTRIBUTING.md" $apioutput
-
+Copy-Item "$($PSScriptRoot)\CODE_OF_CONDUCT.md" $apioutput
+Copy-Item "$($PSScriptRoot)\.github" $apioutput -Recurse
