4970b9054a0c9ab9020a3b46dc0d02d81a90fd2b
EldoBam <amna.wolf@gmail.com>
2025-05-23 02:01:40 +0200

    fixed examples inserting

--- GeneratePowershellCmdlets.ps1
+++ GeneratePowershellCmdlets.ps1
@@ -20,5 +20,5 @@ Copy-Item "$($PSScriptRoot)\LICENSE" $apioutput
 Copy-Item "$($PSScriptRoot)\CONTRIBUTING.md" $apioutput
 Copy-Item "$($PSScriptRoot)\CODE_OF_CONDUCT.md" $apioutput
 Copy-Item "$($PSScriptRoot)\.github" $apioutput -Recurse
-New-Item -Path $apioutput -Name "examples"
-Copy-Item "$($PSScriptRoot)\*" "$($apioutput)\examples\"
+New-Item -Path $apioutput -Name "examples" -ItemType Directory
+Copy-Item "$($PSScriptRoot)\output\examples\*" "$($apioutput)\examples\"
